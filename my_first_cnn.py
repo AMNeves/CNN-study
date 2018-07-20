a223776c8ae3c1ea2fdc52860f65666585df35a3
AMNevesProg <ami.neves.prog@gmail.com>
2018-07-20 02:59:35 +0100

    restart

--- my_first_cnn.py
+++ my_first_cnn.py
@@ -12,7 +12,7 @@ tf.logging.set_verbosity(tf.logging.INFO)
 def cnn_model_fn(features, labels, mode):
   """Model function for the CNN"""
   #Input layer - reshape to 28x28
-  input_layer = tf.reshape(features["X"], [-1, 28 , 28, 1])
+  input_layer = tf.reshape(features["x"], [-1, 28 , 28, 1])
 
   #first convolution - 32 5x5 filters with relu activation function
   convo1 = tf.layers.conv2d(
@@ -23,7 +23,7 @@ def cnn_model_fn(features, labels, mode):
     activation=tf.nn.relu
   )
   #pooling layer 1 - pooling with 2x2 filter and stride of 2. pooled no overlap
-  pool1 = tf.layers.max_pooling2d(input=convo1,pool_size=[2, 2],strides=2)
+  pool1 = tf.layers.max_pooling2d(inputs=convo1,pool_size=[2, 2],strides=2)
 
   #convo2 and pool2
   convo2 = tf.layers.conv2d(
@@ -33,15 +33,15 @@ def cnn_model_fn(features, labels, mode):
     padding="same",
     activation=tf.nn.relu
   )
-  pool2 = tf.layers.max_pooling2d(input=convo2, pool_size=[2, 2], strides=2)
+  pool2 = tf.layers.max_pooling2d(inputs=convo2, pool_size=[2, 2], strides=2)
 
   #dense layer 1
   pool2_flat = tf.reshape(pool2, [-1, 7 * 7 *64])
   dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-  dropout = tf.layers.dropout(input=dense, rate=0.4, training = mode == tf.estimator.ModeKeys.TRAIN)
+  dropout = tf.layers.dropout(inputs=dense, rate=0.4, training = mode == tf.estimator.ModeKeys.TRAIN)
 
   #logits output layer
-  logits = tf.layers.dense(input=dropout, units=10)
+  logits = tf.layers.dense(inputs=dropout, units=10)
 
   predictions = {
     #Generate predictions
@@ -59,15 +59,56 @@ def cnn_model_fn(features, labels, mode):
   #configure training op
   if mode == tf.estimator.ModeKeys.TRAIN:
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-    train_op = otimizer.minimize(
+    train_op = optimizer.minimize(
       loss=loss,
       global_step=tf.train.get_global_step()
     )
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
+  # Add evaluation metrics for EVAL mode
+  eval_metric_ops = {
+      "accuracy": tf.metrics.accuracy(
+          labels=labels, predictions=predictions["classes"])}
+  return tf.estimator.EstimatorSpec(
+      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+def main(unused_argv):
+  # Load training and eval data
+  mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+  train_data = mnist.train.images # Returns np.array
+  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+  eval_data = mnist.test.images # Returns np.array
+  eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+  #estimator
+  mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+
+  #loggin
+  tensor_to_log = {"probabilities" : "softmax_tensor"}
+  logging_hook = tf.train.LoggingTensorHook(tensors=tensor_to_log, every_n_iter=50)
+
+  #training
+
+  train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x= {"x": train_data},
+    y= train_labels,
+    batch_size=100,
+    num_epochs=None,
+    shuffle=True)
+
+  mnist_classifier.train(input_fn=train_input_fn, steps=20000, hooks=[logging_hook])
+  
+  #evaluation
+  eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": eval_data},
+      y=eval_labels,
+      num_epochs=1,
+      shuffle=False)
+  eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+  print(eval_results)
+
+
     
-
-
-
+  
 if __name__ == "__main__":
   tf.app.run()
